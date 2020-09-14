19a223cab17063444d5a6c6bc24edd1e42faf719
Lukasz-Pe <lukaszpe@gmail.com>
2020-09-14 12:41:35 +0200

    resource: added icons for FAB in app module

--- app/src/main/java/com/egm/magazyn/ui/reminders/remindersFragment.java
+++ app/src/main/java/com/egm/magazyn/ui/reminders/remindersFragment.java
@@ -1,5 +1,8 @@
 package com.egm.magazyn.ui.reminders;
 
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+import android.content.Context;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -16,17 +19,26 @@ import com.egm.magazyn.R;
 public class remindersFragment extends androidx.fragment.app.Fragment {
 
     private remindersViewModel remindersViewModel;
+    private Context context;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         remindersViewModel =
                 new ViewModelProvider(this).get(remindersViewModel.class);
         View root = inflater.inflate(R.layout.reminders_layout, container, false);
-        final TextView textView = root.findViewById(R.id.text_reminders);
+        final TextView textView = root.findViewById(R.id.rem_txt);
         remindersViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
             @Override
             public void onChanged(@Nullable String s) {
                 textView.setText(s);
+            }
+        });
+        FloatingActionButton rem_fab = root.findViewById(R.id.reminders_fab);
+        rem_fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
             }
         });
         return root;
--- app/src/main/java/com/egm/magazyn/ui/reminders/remindersViewModel.java
+++ app/src/main/java/com/egm/magazyn/ui/reminders/remindersViewModel.java
@@ -8,15 +8,10 @@ import androidx.lifecycle.MutableLiveData;
 public class remindersViewModel extends androidx.lifecycle.ViewModel {
 
     private MutableLiveData<String> mText;
-    private Context context;
-    private String textToShow="dummy";
     public remindersViewModel() {
 
         mText = new MutableLiveData<>();
         mText.setValue("To jest fragment dla przypominajek");
-    }
-    public void setText(String text){
-        textToShow=text;
     }
     public LiveData<String> getText() {
         return mText;
